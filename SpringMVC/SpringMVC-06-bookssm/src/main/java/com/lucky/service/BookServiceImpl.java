ab65a9870f1d444f84d9004f8af12ac521536824
“liwanyu” <1265781355@qq.com>
2023-06-13 07:08:19 +0800

    springmvc-bookssm学习代码

--- SpringMVC/SpringMVC-06-bookssm/src/main/java/com/lucky/service/BookServiceImpl.java
+++ SpringMVC/SpringMVC-06-bookssm/src/main/java/com/lucky/service/BookServiceImpl.java
@@ -2,17 +2,40 @@ package com.lucky.service;
 
 import com.lucky.dao.BookMapper;
 import com.lucky.pojo.Books;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.util.pattern.PathPattern;
+
+import java.util.List;
 
 public class BookServiceImpl implements BookService {
-    //调用dao层的操作，设置一个set接口，方便spring的管理
+    @Autowired
     private BookMapper bookMapper;
 
-    public void setBookMapper(BookMapper bookMapper) {
-        this.bookMapper = bookMapper;
+    @Override
+    public int addBook(Books books) {
+        return 0;
     }
 
     @Override
-    public int addBook(Books book) {
-        return bookMapper.addBook(book);
+    public int deleteBook(int id) {
+        return 0;
+    }
+
+    @Override
+    public int updateBook(Books books) {
+        return 0;
+    }
+
+    @Override
+    public Books queryBookById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Books> queryAllBook() {
+        return null;
+    }
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
     }
 }
---
+++ SpringMVC/SpringMVC-06-bookssm/web/WEB-INF/jsp/allBook.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>书籍页面</title>
+</head>
+<body>
+<h3><a href="${pageContext.request.contextPath}/book/allBook">进入书籍页面</a> </h3>
+</body>
+</html>
