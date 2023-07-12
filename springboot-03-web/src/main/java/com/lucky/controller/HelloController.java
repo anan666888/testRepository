719c431e616456525166068b46767a057dbb0ea9
liwanyu <1265781355@qq.com>
2023-07-12 08:07:38 +0800

    MVC自动配置原理

--- springboot-03-web/src/main/java/com/lucky/controller/HelloController.java
+++ springboot-03-web/src/main/java/com/lucky/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.lucky.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Controller
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(){
-        return "hello,world!";
+    public String hello(Model model){
+        model.addAttribute("userList", Arrays.asList("liwanyu","zhangsan"));
+        model.addAttribute("msg","<h1>hello,world!!!<h1>");
+        return "welcome";
     }
 }
